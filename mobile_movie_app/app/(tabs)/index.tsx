9469ccff49dbcb5ae26eefdd049fc454b725dacd
daaaanav <dj631125@gmail.com>
2026-02-11 00:13:09 +0530

    Tabs of Apps

---
+++ mobile_movie_app/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+export default function Index() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-5xl font-bold text-dark-200">
+                Welcome To new Jounery!
+            </Text> 
+        </View>
+    );
+}
