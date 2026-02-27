93c2313161bbe76f4a21841f3621c06f534eb8d3
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-02-27 12:20:40 +0530

    Complete Setup For Bulding app

---
+++ Estate_App/app/(root)/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import signIn from "../signIn";
+import explore from "./explore";
+import profile from "./profile";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Welcome to Restate App</Text>
+      <Link href="/signIn">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
