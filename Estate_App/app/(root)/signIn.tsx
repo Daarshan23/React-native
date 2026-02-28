8016f18c072f0c4e509d2b2ac50dd17f2e5fa672
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-02-28 15:14:31 +0530

    Complete Curd OPPS

--- Estate_App/app/(root)/signIn.tsx
+++ Estate_App/app/(root)/signIn.tsx
@@ -1,10 +1,21 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '@/constants/images'
 import icons from '@/constants/icons'
+import { login } from '@/lib/appwrite'
 const signIn = () => {
-  const handelLogin = () => { };
+  const handelLogin = async () => {
+    const result = await login();
+
+    if (result) {
+      console.log('Login Success')
+    }
+    else {
+      Alert.alert('Error, Failed to login')
+    }
+
+  };
   return (
     <SafeAreaView className='bg-white h-full'>
       <ScrollView contentContainerClassName='h-full'>
