59e6140f9ac0874f138b73e5fcdf3ccf93639870
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-03-01 14:48:13 +0530

    Home Page Start

---
+++ Estate_App/app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import icons from '@/constants/icons'
+
+const TabIcon = ({ focused, icon, title }: { focused: boolean; icon: any; title: string; }) => (
+    <View className='flex-1 mt-0 flex flex-col items-center'>
+        <Image source={icon} tintColor={focused ? '#0061ff' : '#666876'} resizeMode='contain' className='size-6'/>
+    </View>
+)
+
+const TabsLayout = () => {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: 'white',
+                    position: 'absolute',
+                    borderTopColor: '#0061FF1A',
+                    borderTopWidth: 1,
+                    minHeight: 70
+                }
+            }}
+        >
+            <Tabs.Screen
+                name='index'
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon icon={icons.home}
+                        focused={focused}
+                        title='Home'
+                        
+                        />
+                    )
+                }}
+            />
+        </Tabs>
+    )
+}
+
+export default TabsLayout
--- Estate_App/app/(root)/signIn.tsx
+++ Estate_App/app/(root)/signIn.tsx
@@ -4,12 +4,21 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '@/constants/images'
 import icons from '@/constants/icons'
 import { login } from '@/lib/appwrite'
+import { useGlobalContext } from '@/lib/globalProvider'
+import { Redirect } from 'expo-router'
+
+
 const signIn = () => {
+
+  const { refetch, loading, isLoggedIn } = useGlobalContext();
+
+  if (!loading && isLoggedIn) return <Redirect href="/" />
+
   const handelLogin = async () => {
     const result = await login();
 
     if (result) {
-      console.log('Login Success')
+      refetch();
     }
     else {
       Alert.alert('Error, Failed to login')
