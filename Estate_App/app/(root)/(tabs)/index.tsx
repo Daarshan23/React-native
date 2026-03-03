92a411340808f6c786c70de64a4073193511fee2
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-03-03 16:10:12 +0530

    Cards Implements

--- Estate_App/app/(root)/(tabs)/index.tsx
+++ Estate_App/app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import signIn from "../../Auth/signIn";
 import explore from "./explore";
 import profile from "./profile";
@@ -7,6 +7,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
+import { Card, FeaturedCard } from "@/components/Cards";
 
 export default function Index() {
   return (
@@ -16,14 +17,24 @@ export default function Index() {
           <View className="flex flex-row items-center">
             <Image source={images.avatar} className="size-12 rounded-full" />
             <View className="flex flex-col items-start ml-2 justify-center">
-                  <Text className="text-xs font-rubik text-black-100">Good Morning </Text>
-                  <Text className="text-base font-rubik-medium text-black-300">Darshan</Text>
+              <Text className="text-xs font-rubik text-black-100">Good Morning </Text>
+              <Text className="text-base font-rubik-medium text-black-300">Darshan</Text>
             </View>
           </View>
-          <Image source={icons.bell} className="size-6"/>
+          <Image source={icons.bell} className="size-6" />
         </View>
-      <Search/>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl text-black-300">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-bold text-primary-300">Sell All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
+      <FeaturedCard/>
+      <Card/>
     </SafeAreaView>
   );
 }
