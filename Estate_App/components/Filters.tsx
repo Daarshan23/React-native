2e8e511edf6339319e5bca604e3e6afe75e47efa
torrenzagraphic-lang <torrenzagraphic@gmil.com>
2026-03-06 16:06:27 +0530

    Check the ui nd change to flatlist

--- Estate_App/components/Filters.tsx
+++ Estate_App/components/Filters.tsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { router, useLocalSearchParams } from 'expo-router'
+import { categories } from '@/constants/data';
 
 const Filters = () => {
+
+  const params = useLocalSearchParams<{filter?: string}>();
+
+  const [SelectedCategory, setSelectedCategory] = useState
+  (params.filter ||'All');
+
+  const handleCategoryPress = (category: string) => {
+    if(SelectedCategory === category){
+      setSelectedCategory:('All');
+      router.setParams({filter:'All'});
+      return;
+    }
+    else
+    {
+      setSelectedCategory(category);
+      router.setParams({filter:category});
+    }
+};
+
   return (
-    <View>
-      <Text>Filters</Text>
-    </View>
+
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} className='mt- mb-2'>
+      {categories.map((item , index) =>(
+        <TouchableOpacity onPress={() => handleCategoryPress(item.category)} className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${SelectedCategory === item.category ? 'bg-primary-300' : 'bg-primary-100 border border-primary-200'}`}>
+          <Text className={`text-sm ${SelectedCategory === item.category ? 'text-white font-bold mt-0.5' : 'text-black-300 font-rubik'}`}>
+            {item.title}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
   )
 }
 
-export default Filters
+export default Filters;
